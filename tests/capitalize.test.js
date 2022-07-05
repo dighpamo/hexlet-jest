0774d8473d7e8b151419a5988a79d57f331754b7
dighpamo <sputikov3150@gmail.com>
2022-07-05 03:29:40 +0300

    push v 0.01

---
+++ tests/capitalize.test.js
@@ -0,0 +1,7 @@
+import { strict as assert } from 'assert';
+import  capitalize  from '../src/capitalize.js';
+
+assert.equal(capitalize(''), '');
+assert.equal(capitalize('hello'), 'Hello');
+
+console.log('All tests passed!');
